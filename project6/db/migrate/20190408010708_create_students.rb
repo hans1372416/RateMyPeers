e5138d8bb34cad811c71661892d56f28785dd4b1
Adlan Azhar <adlanamirul@gmail.com>
2019-04-14 13:00:39 -0400

    updated student and instructor model to fit with group model

--- project6/db/migrate/20190408010708_create_students.rb
+++ project6/db/migrate/20190408010708_create_students.rb
@@ -3,7 +3,9 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     create_table :students do |t|
       t.string :name
       t.string :username
-      t.string :groupName
+      t.string :gname
+      t.string :cname
+      t.string :ratings
 
       t.timestamps
     end
