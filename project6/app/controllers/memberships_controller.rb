91104e9777046d98fe4ad46d305ffc0db09a062c
aiftikhar19 <25934283+aiftikhar19@users.noreply.github.com>
2019-04-14 15:33:13 -0400

    Update memberships_controller.rb

--- project6/app/controllers/memberships_controller.rb
+++ project6/app/controllers/memberships_controller.rb
@@ -64,6 +64,11 @@ class MembershipsController < ApplicationController
   end
 
   private
+   # Use callbacks to share common setup or constraints between actions.
+    def set_membership
+      @membership = Membership.find(params[:id])
+    end
+  
     # Never trust parameters from the scary internet, only allow the white list through.
     def membership_params
       params.require(:membership).permit(:user_id, :group_id)
