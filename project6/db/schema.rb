e32d8b1027494057a2338d63c82f62801bdf103d
Shawn <932512431c@gmail.com>
2019-04-14 19:37:25 -0400

    databse updated

--- project6/db/schema.rb
+++ project6/db/schema.rb
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(version: 2019_04_14_184404) do
   create_table "memberships", force: :cascade do |t|
     t.string "user_id"
     t.string "group_id"
+    t.integer "people_number"
+    t.integer "m1"
+    t.integer "m2"
+    t.integer "m3"
+    t.integer "m4"
+    t.integer "m5"
+    t.integer "m6"
+    t.integer "m7"
+    t.integer "m8"
+    t.integer "m9"
+    t.integer "m10"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
